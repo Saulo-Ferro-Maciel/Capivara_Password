4f404547dc32b2bce4344c55743d0e5e5becc204
Saulo Ferro Maciel <102975817+Saulo-Ferro-Maciel@users.noreply.github.com>
2022-06-28 01:51:26 -0300

    Update backend_Capivara_Password.py

--- Project Code/backend/backend_Capivara_Password.py
+++ Project Code/backend/backend_Capivara_Password.py
@@ -805,3 +805,15 @@ class App_principal(md_application):
 if __name__ == '__main__':
     project = App_principal()
     project.run()
+
+   
+
+"""
+Copyright 2022 Saulo Ferro Maciel
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+"""
