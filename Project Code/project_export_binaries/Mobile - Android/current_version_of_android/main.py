5b8c45e18c81c79b45e13eb91ab71cb4da69ad2e
Saulo Ferro Maciel <102975817+Saulo-Ferro-Maciel@users.noreply.github.com>
2022-06-28 01:55:08 -0300

    Update main.py

--- Project Code/project_export_binaries/Mobile - Android/current_version_of_android/main.py
+++ Project Code/project_export_binaries/Mobile - Android/current_version_of_android/main.py
@@ -1,3 +1,14 @@
+"""
+Copyright 2022 Saulo Ferro Maciel
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+"""
+
+
 import sqlite3 as sq3
 import webbrowser as web
 from random import randint as ran
