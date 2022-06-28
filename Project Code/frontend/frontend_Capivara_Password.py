564ac3817bbbdcc39221bbb07162cfc4152d8e14
Saulo Ferro Maciel <102975817+Saulo-Ferro-Maciel@users.noreply.github.com>
2022-06-28 01:53:48 -0300

    Update frontend_Capivara_Password.py

--- Project Code/frontend/frontend_Capivara_Password.py
+++ Project Code/frontend/frontend_Capivara_Password.py
@@ -589,3 +589,13 @@ MDNavigationLayout:
 
              
 '''
+
+"""
+Copyright 2022 Saulo Ferro Maciel
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+"""
